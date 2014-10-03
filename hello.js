1223a5ebf805ce52bdb790a507a4dd07ac0ec7e1
christinaleuci <christinaleuci@gmail.com>
2014-10-02 20:24:44 -0400

    deleted README and hello world

---
+++ hello.js
@@ -0,0 +1,11 @@
+var http = require('http'); //requires the module
+
+http.createServer(function(request, response){
+  response.writeHead(200); //status code in header
+  response.write("Hello World!!"); //response body
+  response.end(); //closes connection
+}).listen(8080); //listens for connections on this port
+console.log('Listening on port 8080');
+
+//event loop
+//checks if request comes in then run callback
